c4dcef75ba0b7c4ebdc09173095a450193d09c33
weiguo.ni <neefrankie@gmail.com>
2020-04-27 09:27:29 +0800

    init

---
+++ src/App.svelte
@@ -0,0 +1,113 @@
+<script lang="typescript">
+  import Nested from './Nested.svelte';
+  import Thing from './Thing.svelte';
+  import Inner from './Inner.svelte';
+
+  export let name;
+  const src = "http://placekitten.com/200/300";
+
+  let count = 0;
+
+  $: doubled = count * 2;
+
+  function handleClick() {
+    count += 1;
+  }
+
+  console.log(`the count is ${count}`);
+
+  let user = {
+    loggedIn: false,
+  }
+
+  function toggle() {
+    user.loggedIn = !user.loggedIn;
+  }
+
+  let cats = [
+    { id: 'J---aiyznGQ', name: 'Keyboard Cat' },
+		{ id: 'z_AbfPXTKms', name: 'Maru' },
+		{ id: 'OUtn3pvWmpg', name: 'Henri The Existential Cat' }
+  ];
+
+  let things = [
+    { id: 1, color: '#0d0887' },
+		{ id: 2, color: '#6a00a8' },
+		{ id: 3, color: '#b12a90' },
+		{ id: 4, color: '#e16462' },
+		{ id: 5, color: '#fca636' }
+  ]
+
+  function handleRemove() {
+    things = things.slice(1);
+  }
+
+  let m = {x: 0, y: 0};
+  function handleMouseover(event) {
+    m.x = event.clientX;
+    m.y = event.clientY;
+  }
+
+  function handleMessage(event) {
+    alert(event.detail.text);
+  }
+</script>
+
+<main>
+  {#if user.loggedIn}
+    <button on:click={toggle}>Log out</button>
+  {:else}
+    <button on:click={toggle}>Log in</button>
+  {/if}
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+  <div on:mouseover="{handleMouseover}">
+    The mouse position is {m.x} x {m.y}
+  </div>
+  <img src={src} alt="A kitten {name}">
+  <Nested answer={42}/>
+  <Nested/>
+
+  <button on:click={handleClick}>Clicked {count} {count === 1 ? 'time' : 'times'}</button>
+
+  <p>{count} doubled is {doubled}</p>
+
+  <ul>
+    {#each cats as cat, i}
+      <li>
+        <a href="src">{i + 1}: {cat.name}</a>
+      </li>
+    {/each}
+  </ul>
+
+  <button on:click="{handleRemove}">Remove first thing</button>
+
+  {#each things as thing (thing.id)}
+    <Thing current={thing.color}/>
+  {/each}
+
+  <Inner on:message={handleMessage}/>
+</main>
+
+<style lang="scss">
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Thing.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let current;
+  const initial = current;
+</script>
+
+<p>
+  <span style="background-color: {initial}">initial</span>
+  <span style="background-color: {current}">current</span>
+</p>
