93399d61d67932df4d48711ffeb8591cf4f81920
weiguo.ni <neefrankie@gmail.com>
2020-05-28 23:14:08 +0800

    login form

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
-import App from './App.svelte';
+// import App from './App.svelte';
 
-const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+// const app = new App({
+// 	target: document.body,
+// 	props: {
+// 		name: 'world'
+// 	}
+// });
+
+import Login from './pages/Login.svelte';
+
+const app = new Login({
+  target: document.body
 });
 
 export default app;
---
+++ src/pages/Login.svelte
@@ -0,0 +1,42 @@
+<script lang="typescript">
+
+let credentials = {
+  email: '',
+  password: '',
+};
+
+let errors = {
+  email: '',
+  password: ''
+}
+
+function submit() {
+  console.log(credentials);
+  errors = {
+    email: 'Invalid email',
+    password: 'Invalid password'
+  };
+}
+</script>
+
+<h2>Login</h2>
+
+<form on:submit|preventDefault={submit}>
+  <div>
+    <label for="email">Email</label>
+    <input type="text" id="email" bind:value={credentials.email}/>
+    {#if errors.email}
+      <div>{errors.email}</div>
+    {/if}
+  </div>
+
+  <div>
+    <label for="password">Password</label>
+    <input type="password" id="password" bind:value={credentials.password}/>
+    {#if errors.password}
+      <div>{errors.password}</div>
+    {/if}
+  </div>
+
+  <button type="submit">Submit</button>
+</form>
